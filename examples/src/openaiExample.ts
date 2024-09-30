844c649d89fcb863301a83f38554794c7a253cb6
Andrea <andrea.manna@etiqa.it>
2024-09-30 18:15:01 +0200

    reorganizing runTools

--- examples/src/openaiExample.ts
+++ examples/src/openaiExample.ts
@@ -1,50 +1,39 @@
 import OpenAI from 'openai';
-import Toolhouse from '@toolhouseai/toolhouse-sdk-typescript';
+import Toolhouse, { type OpenAiTools, type OpenAiChatCompletionMessage } from '@toolhouseai/toolhouse-sdk-typescript';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new OpenAI({
-  apiKey: process.env['OPENAI_API_KEY']
-});
+async function main() {
+  const toolhouse = new Toolhouse({
+    baseUrl: 'https://g6dywws9a0.execute-api.us-west-2.amazonaws.com/v1'
+  })
 
-const toolhouse = new Toolhouse({
-  baseUrl: 'https://g6dywws9a0.execute-api.us-west-2.amazonaws.com/v1'
-})
+  const client = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY']
+  })
 
-type Tools = OpenAI.Chat.Completions.ChatCompletionTool[] | undefined
-const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [{ role: 'user', content: 'Search information about Etiqa s.r.l' }];
+  const messages: OpenAiChatCompletionMessage[] = [{ role: 'user', content: 'Search information about Etiqa s.r.l' }]
 
-async function main1() {
-  const allTools = await toolhouse.getTools()
+  const tools = await toolhouse.getTools() as OpenAiTools
 
   const chatCompletion = await client.chat.completions.create({
     messages,
     model: 'gpt-3.5-turbo',
-    tools: allTools as Tools
+    tools
   })
 
-  console.log(chatCompletion)
-  // const {data: list} = await toolhouse.runTools(chatCompletion, true)
+  const openAiMessage = await toolhouse.runTools(chatCompletion)
+  // console.log(JSON.stringify(openAiMessage))
+  const newMessages = [...messages, { ...openAiMessage!.content }] as OpenAiChatCompletionMessage[]
+  console.log(JSON.stringify(newMessages))
 
-  // unisco i messaggi di run tools con messages  in nuovaLista
-  // const chatCompletion = await client.chat.completions.create({
-  //   messages: nuovaLista,
+  // const chatCompleted = await client.chat.completions.create({
+  //   messages: newMessages,
   //   model: 'gpt-3.5-turbo',
-  //   tools: allTools as Tools
-  // }) 
-  // ritorna chatCompletion
-}
-async function main2() {
-  const bundleTools = await toolhouse.getTools('bundle1')
-  const chatCompletion = await client.chat.completions.create({
-    messages,
-    model: 'gpt-3.5-turbo',
-    tools: bundleTools as Tools
-  })
-
-  console.log(chatCompletion)
+  //   tools
+  // })
+  // console.log(JSON.stringify(chatCompleted))
 }
 
-main1();
-main2();
+main();
