47cfc1107b0698b8f33fa1d523ccdebef462bc29
orliesaurus <orlando@opensauce.it>
2025-01-02 22:17:13 -0600

    refactor: replace Axios with Fetch API for HTTP requests and add LineDecoder utility

--- src/http/error.ts
+++ src/http/error.ts
@@ -1,12 +1,14 @@
-import { HttpMetadata } from './types';
+import { HttpMetadata } from "./types";
 
 export class HttpError extends Error {
   public readonly error: string;
   public readonly metadata: HttpMetadata;
+  public readonly raw?: ArrayBuffer;
 
-  constructor(metadata: HttpMetadata, error?: string) {
+  constructor(metadata: HttpMetadata, raw?: ArrayBuffer, error?: string) {
     super(error);
     this.error = metadata.statusText;
     this.metadata = metadata;
+    this.raw = raw;
   }
 }
